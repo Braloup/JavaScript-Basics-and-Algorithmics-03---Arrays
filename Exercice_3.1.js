286021871abaa4c484f0b3b552889e2f94a8b1a8
VieuxLoupGris <provocemois@gmail.com>
2019-01-29 13:30:55 +0100

    Add documentation and comment

--- Exercice_3.1.js
+++ Exercice_3.1.js
@@ -1,17 +1,24 @@
-const readLineSync = require("readline-sync");
+/*
+**Write a program that will add all the elements of an array.
+*/
 
-let userNumber = readLineSync.question("How many numbers do you want to add?: ");
+const readLineSync = require("readline-sync");//Call the module to ask question to the user.
+
+let userNumber = readLineSync.question("How many numbers do you want to add?: ");//How many numbers want the user?
+
 let addNumber = [];
+
 while (userNumber > 0) {
 
   let anotherNumber = readLineSync.question("Enter the number you want to add: ");
   addNumber.push(parseInt(anotherNumber));
   userNumber -= 1;
 }
-console.log("Your choice number is: " + addNumber);
+
+console.log("Your choice number is: " + addNumber);// An array withe the number user choice.
 let toatalNumber = 0;
 
-for (var i = 0; i < addNumber.length; i++) {
+for (var i = 0; i < addNumber.length; i++) {//Add the all numbers in the array
   toatalNumber += addNumber[i];
 }
 console.log("The addition of these numbers gives: " + toatalNumber);
